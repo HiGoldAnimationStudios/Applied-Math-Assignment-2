020dcb033576a6f8a5a40232fe75ab85644ea378
HiGoldAnimationStudios <higoldanimationstudios@gmail.com>
2025-09-21 22:22:29 -0400

    part 3

---
+++ approximate_jacobian.m
@@ -0,0 +1,18 @@
+%Implementation of finite difference approximation
+%for Jacobian of multidimensional function
+%INPUTS:
+%fun: the mathetmatical function we want to differentiate
+%x: the input value of fun that we want to compute the derivative at
+%OUTPUTS:
+%J: approximation of Jacobian of fun at x
+function J = approximate_jacobian(fun,x)
+
+    
+    J = zeros(length(x),length(x));
+
+    for i=1:length(x)
+        dfdx=approximate_derivative(fun,x(i));
+        J(:,i)=dfdx;
+    end
+    
+end
---
+++ jacobian_test_function.m
@@ -0,0 +1,8 @@
+%computes a quadratic function on input X
+function f_val = jacobian_test_function(X,A,B,C)
+    output_length = length(C);
+    f_val = B*X+C;
+    for n = 1:output_length
+    f_val(n)=f_val(n)+(X'*A(:,:,n)*X);
+    end
+end
